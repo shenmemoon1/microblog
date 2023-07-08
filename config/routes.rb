3748033afcec4fcf264362cffba7f6f1117531d8
billlims <davidlinxf@gmail.com>
2023-07-09 01:59:36 +1000

    #update navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # get '/', to: 'main#index'
+  root to: 'main#index'
+  # get '/about-us', to: 'about#index', as: :about
+  get '/about', to: 'about#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
